3f29772b3a0e49f5159174c1537da13efa2bc27b
Casra Ghazanfari <36279442+casra02@users.noreply.github.com>
2023-05-01 11:37:13 -0400

    Update AdditionUnitTest.py

--- AdditionUnitTest.py
+++ AdditionUnitTest.py
@@ -5,7 +5,9 @@ class TestAdditionMethods(unittest.TestCase):
     def test_1(self):
         summ = additionTest(3,4)
         summ.assertEqual(7)
+        print(test_1 success)
 
     def test_2(self):
         summ = additionTest(-5,6)
         summ.assertEqual(1)
+        print(test_2 success)
